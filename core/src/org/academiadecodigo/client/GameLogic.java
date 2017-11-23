294ba84bc4101b67803b9b400bbccb9fbd65cb09
Luis Alves <luisfiliper.alves@gmail.com>
2017-11-23 21:46:26 +0000

    update assets folder

--- core/src/org/academiadecodigo/client/GameLogic.java
+++ core/src/org/academiadecodigo/client/GameLogic.java
@@ -5,15 +5,23 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.academiadecodigo.client.screens.PlayScreen;
 
 public class GameLogic extends Game {
 
     SpriteBatch batch;
+    PlayScreen playScreen;
+    public final static int WIDTH = 800;
+    public final static int HEIGHT = 640;
+
+
 
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+
+        playScreen = new PlayScreen(this);
     }
 
     @Override
@@ -23,10 +31,10 @@ public class GameLogic extends Game {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         //updates
-
+        playScreen.update(0);
         // drawing
         batch.begin();
-
+        playScreen.render(0);
         batch.end();
     }
 
--- desktop/src/org/academiadecodigo/desktop/DesktopLauncher.java
+++ desktop/src/org/academiadecodigo/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package org.academiadecodigo.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import org.academiadecodigo.GameLogic;
+import org.academiadecodigo.client.GameLogic;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
