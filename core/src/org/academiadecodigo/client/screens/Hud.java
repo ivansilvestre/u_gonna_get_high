194e2c0cf1f45dbfe65e9c7885148aa2f9053d3b
carlos fonseca <carlosfonseca@MacBook-Pro-de-carlos.local>
2017-11-24 08:20:07 +0000

    hud's with colors and dope level

--- core/src/org/academiadecodigo/client/screens/Hud.java
+++ core/src/org/academiadecodigo/client/screens/Hud.java
@@ -1,7 +1,9 @@
 package org.academiadecodigo.client.screens;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -14,8 +16,13 @@ import com.badlogic.gdx.utils.viewport.Viewport;
  */
 public class Hud implements Disposable {
 
+    private Label cenas_DopeLevel;
+    private int cenas_dope;
     private int p2_score = 0;
     private int p1_score = 0;
+
+    private int p1_dope = 100;
+    private int p2_dope = 100;
 
     //Scene2D.ui Stage and its own Viewport for HUD
     public Stage stage;
@@ -28,15 +35,16 @@ public class Hud implements Disposable {
     private int dopeLevel;
     private SpriteBatch sb;
 
-
     //Scene2D widgets
+
     private Label p2_scoreLabel;
     private Label p1_scoreLabel;
+    private Label p1_DopeLevel;
+    private Label p2_DopeLevel;
     private Label player2;
     private Label timer;
     private Label TIME;
     private Label player1;
-
     public Hud(SpriteBatch sb) {
         //define our tracking variables
 
@@ -62,10 +70,13 @@ public class Hud implements Disposable {
         table.setFillParent(true);
 
         //define our labels using the String, and a Label style consisting of a font and color
-        p2_scoreLabel = new Label(String.format("%01d", p1_score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        p1_scoreLabel = new Label(String.format("%01d", p2_score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        player2 = new Label("PLAYER 2", new Label.LabelStyle(new BitmapFont().getData().setScale(1f, 1f), Color.WHITE));
-        timer = new Label(String.format("%03d",worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        p2_scoreLabel = new Label(String.format("%01d", p2_score), new Label.LabelStyle(new BitmapFont(), Color.GREEN));
+        p1_scoreLabel = new Label(String.format("%01d", p1_score), new Label.LabelStyle(new BitmapFont(), Color.GREEN));
+        p2_DopeLevel = new Label(String.format("%01d", p2_dope), new Label.LabelStyle(new BitmapFont(), Color.YELLOW));
+        p1_DopeLevel = new Label(String.format("%01d", p1_dope), new Label.LabelStyle(new BitmapFont(), Color.YELLOW));
+        cenas_DopeLevel = new Label(String.format("", cenas_dope), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        player2 = new Label("PLAYER 2", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        timer = new Label(String.format("%03d",worldTimer), new Label.LabelStyle(new BitmapFont(), Color.RED));
         TIME = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         player1 = new Label("PLAYER 1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
@@ -80,6 +91,10 @@ public class Hud implements Disposable {
         table.add(timer).expandX();
         table.add(p2_scoreLabel).expandX();
 
+        table.row();
+        table.add(p1_DopeLevel);
+        table.add(cenas_DopeLevel).expandX();
+        table.add(p2_DopeLevel).expandX();
         //add our table to the stage
         stage.addActor(table);
 
@@ -98,19 +113,18 @@ public class Hud implements Disposable {
                 timeCount = 0;
             }
         }
-/*
-        public static void addScore(int value){
-            money += value;
-            p1_scoreLabel.setText(String.format("%06d", money));
-        }
-  */
+
+    /*
+            public static void addScore(int value){
+                money += value;
+                p1_scoreLabel.setText(String.format("%06d", money));
+            }
+      */
     @Override
     public void dispose() {
         stage.dispose();
     }
-
     public void draw() {
 
     }
-
 }
