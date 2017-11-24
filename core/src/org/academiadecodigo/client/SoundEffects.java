ce113197057da07dc7a5d007d2619faf77f94a9c
Ivan <ivanbsilvestre@gmail.com>
2017-11-24 06:29:09 +0000

    sound effects

--- core/src/org/academiadecodigo/client/SoundEffects.java
+++ core/src/org/academiadecodigo/client/SoundEffects.java
@@ -11,22 +11,44 @@ import com.badlogic.gdx.audio.Sound;
 public class SoundEffects {
 
     private Music mainSound;
+
     private Sound smokeWeed;
     private Sound cops;
+    private Sound catchItem;
+    private Sound smokeWeedEveryday;
+    private Sound fWhereIsMyDealer;
+    private Sound gettingSober;
+    private Sound yeah;
+
 
     public void playMusic() {
 
         mainSound = Gdx.audio.newMusic(Gdx.files.internal("sound/becauseIGotHigh.mp3"));
-        mainSound.play();
-        mainSound.setLooping(true);
+        //  mainSound.play();
+        // mainSound.setLooping(true);
     }
 
     public void playSound() {
 
+        //WITH WEED
         smokeWeed = Gdx.audio.newSound(Gdx.files.internal("sound/inspire.mp3"));
-        smokeWeed.play();
+        //smokeWeed.play();
+        smokeWeedEveryday = Gdx.audio.newSound(Gdx.files.internal("sound/smokeWeedEveryDay.mp3"));
+        //smokeWeedEveryday.play();
+        yeah = Gdx.audio.newSound(Gdx.files.internal("sound/yeah.mp3"));
+        //yeah.play();
+
+        //MISSING WEED
+        fWhereIsMyDealer = Gdx.audio.newSound(Gdx.files.internal("sound/fWhereIsMyDealer.mp3"));
+        //fWhereIsMyDealer.play();
+        gettingSober = Gdx.audio.newSound(Gdx.files.internal("sound/gettingSober.mp3"));
+        //gettingSober.play();
+
 
         cops = Gdx.audio.newSound(Gdx.files.internal("sound/siren.mp3"));
-        cops.play();
+        // cops.play();
+
+        catchItem = Gdx.audio.newSound(Gdx.files.internal("sound/catchItem.mp3"));
+        // catchItem.play();
     }
 }
