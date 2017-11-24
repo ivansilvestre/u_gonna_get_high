ffdaabbb303a6cc847a283bb2dbacb25c50718b8
Luis Alves <luisfiliper.alves@gmail.com>
2017-11-24 02:35:42 +0000

    phone, chore, wee d, and gameobjectFactory

--- core/src/org/academiadecodigo/client/objects/GameObjects.java
+++ core/src/org/academiadecodigo/client/objects/GameObjects.java
@@ -1,11 +1,25 @@
 package org.academiadecodigo.client.objects;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+
 /**
  * Created by codecadet on 23/11/17.
  */
-public class GameObjects {
+public abstract class GameObjects {
 
-    public void connect(){
+    private float posX;
+    private float posY;
+
+
+    public GameObjects(float posX, float posY) {
+
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void collect(){
 
     }
 
--- core/src/org/academiadecodigo/client/objects/ObjectFactory.java
+++ core/src/org/academiadecodigo/client/objects/ObjectFactory.java
@@ -5,6 +5,34 @@ package org.academiadecodigo.client.objects;
  */
 public abstract class ObjectFactory {
 
+    public static Chore createChore(float posX, float posY) {
+        Chore chore = null;
 
+        double difficulty = Math.random();
+
+        if (difficulty <= 0.70) {
+
+            int lowDif = (int) (Math.random() * 3 + 1);
+            chore = new Chore(ChoreType.values()[lowDif], posX, posY);
+        }
+
+        if (difficulty > 0.70) {
+
+            chore = difficulty > 0.90 ?
+                    new Chore(ChoreType.CLEAN_TOILET, posX, posY) : new Chore(ChoreType.WASH_DISHES, posX, posY);
+        }
+        return chore;
+    }
+
+
+    public static Phone createPhone(float posX, float posY) {
+
+        return new Phone(posX, posY);
+    }
+
+    public static Weed creatWeed(float posX, float posY) {
+
+        return new Weed(posX, posY);
+    }
 
 }
